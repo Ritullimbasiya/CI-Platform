e9866a3973c51cc8ba130de2425337784d30472e
Ritullimbasiya <ritul@gmail.com>
2022-11-28 08:57:52 +0530

    Admin User CRUD done

--- CI-Plateform project/CI-Plateform/Controllers/AdminController.cs
+++ CI-Plateform project/CI-Plateform/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using CI_Plateform.DbModels;
 using CI_Plateform.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CI_Plateform.Controllers
 {
@@ -8,23 +9,99 @@ namespace CI_Plateform.Controllers
     {
         public ClPlatformContext _db = new ClPlatformContext();
 
+        #region User
         public IActionResult aUser()
         {
             var user = new DatabaseUserViewModel();
             user.users = _db.Users.ToList();
             return View(user);
         }
-        public IActionResult aUseradd()
+
+        [HttpGet]
+        public IActionResult aUseradd(int id = 0)
         {
-            return View();
+            UserVm userVM = new UserVm()
+            {
+                User = new User(),
+                CityList = _db.Cities.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.CityId.ToString()
+                }),
+                CountryList = _db.Countries.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.CountryId.ToString()
+                })
+            };
+            return View(userVM);
+        }
+
+        [HttpPost]
+        public IActionResult aUseradd(UserVm userVM)
+        {
+            if (userVM != null)
+            {
+                User user = new User();
+
+                user.FirstName = userVM.User.FirstName;
+                user.LastName = userVM.User.LastName;
+                user.Email = userVM.User.Email;
+                if (userVM.User.Password != null)
+                    user.Password = userVM.User.Password;
+                user.PhoneNumber = userVM.User.PhoneNumber;
+                user.Avatar = userVM.User.Avatar;
+                user.WhyIVolunteer = userVM.User.WhyIVolunteer;
+                user.EmployeeId = userVM.User.EmployeeId;
+                user.Department = userVM.User.Department;
+                user.CityId = userVM.City.CityId;
+                user.CountryId = userVM.Country.CountryId;
+                user.ProfileText = userVM.User.ProfileText;
+                user.WhyIVolunteer = userVM.User.WhyIVolunteer;
+                user.LinkedInUrl = userVM.User.LinkedInUrl;
+                user.Title = userVM.User.Title;
+                user.Status = 1;
+                user.CreatedAt = DateTime.Now;
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return RedirectToAction("aUser", "Admin");
+            }
+            else
+                return NotFound();
+        }
+        [HttpGet]
+        public IActionResult aUseredit(int? id)
+        {
+            var user = _db.Users.FirstOrDefault(x => x.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
         [HttpPost]
-        public IActionResult aUseradd(User obj)
+        public IActionResult aUseredit(User obj)
         {
-            _db.Users.Add(obj);
+            _db.Users.Update(obj);
             _db.SaveChanges();
             return RedirectToAction("aUser", "Admin");
         }
+        
+        public IActionResult aUserdelete(int? id)
+        {
+            var user = _db.Users.FirstOrDefault(x => x.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            _db.Users.Remove(user);
+            _db.SaveChanges();
+            return RedirectToAction("aUser", "Admin");
+        }
+
+        #endregion User
+
+        #region CMS
         public IActionResult aCMS()
         {
             var cmsPage = new DatabaseUserViewModel();
@@ -35,6 +112,9 @@ namespace CI_Plateform.Controllers
         {
             return View();
         }
+        #endregion CMS
+
+        #region Mission
         public IActionResult aMission()
         {
             var mission = new DatabaseUserViewModel();
@@ -45,6 +125,9 @@ namespace CI_Plateform.Controllers
         {
             return View();
         }
+        #endregion Mission
+
+        #region Skill
         public IActionResult aSkill()
         {
             var skill = new DatabaseUserViewModel();
@@ -55,6 +138,9 @@ namespace CI_Plateform.Controllers
         {
             return View();
         }
+        #endregion Skill
+
+        #region Theme
         public IActionResult aTheme()
         {
             var theme = new DatabaseUserViewModel();
@@ -65,6 +151,9 @@ namespace CI_Plateform.Controllers
         {
             return View();
         }
+        #endregion Theme
+
+        #region Application
         public IActionResult aApplication()
         {
             var application = new DatabaseUserViewModel();
@@ -100,6 +189,9 @@ namespace CI_Plateform.Controllers
 
 
         }
+        #endregion Application
+
+        #region Story
         public IActionResult aStory()
         {
             var story = new DatabaseUserViewModel();
@@ -110,11 +202,15 @@ namespace CI_Plateform.Controllers
         {
             return View();
         }
+
+        #endregion Story
+
+        #region Management
         public IActionResult aManagement()
         {
             return View();
         }
-
+        #endregion Management
 
     }
 }
---
+++ CI-Plateform project/CI-Plateform/Models/UserVm.cs
@@ -0,0 +1,14 @@
+﻿using CI_Plateform.DbModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CI_Plateform.Models
+{
+    public class UserVm
+    {
+        public User? User { get; set; } = null!;
+        public City? City { get; set; } = null;
+        public Country? Country { get; set; } = null;
+        public IEnumerable<SelectListItem> CityList { get; set; }
+        public IEnumerable<SelectListItem> CountryList { get; set; }
+    }
+}
